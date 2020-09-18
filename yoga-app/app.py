6b682e2e6e3e82e7effa93b86b9ba9585c4838ec
Jennifer O'Shea <jenniferoshea@Jennifers-MacBook-Pro.local>
2020-09-18 15:53:09 -0600

    add

--- yoga-app/app.py
+++ yoga-app/app.py
@@ -71,8 +71,8 @@ def predict():
     # Save the file to ./uploads
     basepath = os.path.dirname(__file__)
     file_path = os.path.join(basepath, 'tmping', secure_filename(f.filename))
-    #f.save(file_path)
-    rotate_save(f, file_path)
+    f.save(file_path)
+    #rotate_save(f, file_path)
 
     # Make prediction
     preds = get_category(file_path, model)
