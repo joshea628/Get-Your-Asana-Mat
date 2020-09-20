1acacb7fc758c516be3c8b0974e2a8fa703b67bd
Jennifer O'Shea <jenniferoshea@Jennifers-MacBook-Pro.local>
2020-09-20 12:31:20 -0600

    app updates

--- yoga-app/app.py
+++ yoga-app/app.py
@@ -12,57 +12,22 @@ from tensorflow.keras.applications.xception import preprocess_input
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array, load_img
 from werkzeug.utils import secure_filename
-from os.path import join, dirname, realpath
-
-UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), 'static/temp')
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-def rotate_save(f, file_path):
-    try:
-        image=Image.open(f)
-        for orientation in ExifTags.TAGS.keys():
-            if ExifTags.TAGS[orientation]=='Orientation':
-                break
-        exif=dict(image._getexif().items())
-
-        if exif[orientation] == 3:
-            image=image.rotate(180, expand=True)
-        elif exif[orientation] == 6:
-            image=image.rotate(270, expand=True)
-        elif exif[orientation] == 8:
-            image=image.rotate(90, expand=True)
-        image.save(file_path)
-        image.close()
-
-    except (AttributeError, KeyError, IndexError):
-        # cases: image don't have getexif
-        image.save(file_path)
-        image.close()
-
-        pass
-
-def process_img(filename):
-    original = load_img(filename, target_size = (299,299))
-    numpy_image = preprocess_input( img_to_array(original))
-    image_batch = np.expand_dims(numpy_image, axis =0)
-    return image_batch
+def format(filename):
+    im = load_img(filename, target_size = (299,299))
+    image = preprocess_input(img_to_array(im))
+    return np.expand_dims(image, axis=0)
 
 def get_category(img_path,model):
-    im =  process_img(img_path)
-    preds =  model.predict(im)
-    top_3 = preds.argsort()[0][::-1][:3] # sort in reverse order and return top 3 indices
+    im = format(img_path)
+    pred = model.predict(im)
+    top_3 = pred.argsort()[0][::-1][:3]
     top_3_names = class_names[top_3]
     top_3_percent = preds[0][[top_3]]*100
     top_3_text = '<br>'.join([f'{name}: {percent:.2f}%' for name, percent in zip(top_3_names,top_3_percent)])
     return top_3_text
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
 
 @app.route('/', methods=['GET'])
 def index():
@@ -73,36 +38,20 @@ def index():
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():     
    if request.method == 'POST':
-        # # check if the post request has the file part
-        # if 'file' not in request.files:
-        #     flash('No file part')
-        #     return redirect(request.url)
         file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        # if file.filename == '':
-        #     flash('No selected file')
-        #     return redirect(request.url)
-        # if file and allowed_file(file.filename):
         if file.filename != '':
-            #filename = secure_filename(file.filename)
-            #file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            file.save(secure_filename(file.filename))
-            # Make prediction
+            filename = secure_filename(file.filename)
+            file.save(filename)
+            # Make and display predictions
             preds = get_category(secure_filename(file.filename), model)
-
-            # Delete it so we don't clutter our server up
-            #os.remove(file_path)
-
+            os.remove(filename)
         return preds 
 
 if __name__ == '__main__':
-    #MODEL_PATH = 
     with open ('models/classes.pkl', 'rb') as f:
         class_names = np.array(pickle.load(f))
 
     model = load_model('models/88.5dmhc.h5')
-    #model._make_predict_function()
     print('Model loaded. Start serving...')
 
     #app.run(host='0.0.0.0',port=8105,debug=True)
