98a1f1b6574a90037da57364fd24a0b841ef4d32
Jennifer O'Shea <jenniferoshea@Jennifers-MacBook-Pro.local>
2020-09-20 12:11:41 -0600

    ad

--- yoga-app/app.py
+++ yoga-app/app.py
@@ -73,25 +73,26 @@ def index():
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():     
    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
+        # # check if the post request has the file part
+        # if 'file' not in request.files:
+        #     flash('No file part')
+        #     return redirect(request.url)
         file = request.files['file']
         # if user does not select file, browser also
         # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
+        # if file.filename == '':
+        #     flash('No selected file')
+        #     return redirect(request.url)
+        # if file and allowed_file(file.filename):
+        if file.filename != '':
+            #filename = secure_filename(file.filename)
+            #file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            file.save(secure_filename(file.filename))
             # Make prediction
             preds = get_category(file_path, model)
 
             # Delete it so we don't clutter our server up
-            os.remove(file_path)
+            #os.remove(file_path)
 
         return preds 
 
